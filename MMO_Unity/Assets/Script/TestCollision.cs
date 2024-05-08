77ab4a6ee1f0a2ff5814cb6eb6950bc48a22fc7e
Johnny <pgisung@naver.com>
2024-05-08 23:46:32 +0900

    [Chore] training collision, collider and raycasting

--- MMO_Unity/Assets/Script/TestCollision.cs
+++ MMO_Unity/Assets/Script/TestCollision.cs
@@ -25,6 +25,56 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        
+        // Local <-> World <-> ( Viewport <-> Screen ) (화면)
+
+        //Debug.Log(Input.mousePosition); // Screen 화면 픽셀 좌표
+
+        //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport 화면에서의 비율 최소 0 최대 1
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            // 클래스 이용 레이어 마스크
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+            // 비트 연산자 레이어 마스크
+            //int mask = (1 << 8) | (1 << 9);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, mask))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.tag}");
+            }
+        }
     }
 }
+
+// ----- Raycast 개념
+//Vector3 look = transform.TransformDirection(Vector3.forward);
+//Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+//RaycastHit[] hits;
+//hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+
+//foreach (RaycastHit hit in hits)
+//{
+//    Debug.Log($"Raycast {hit.collider.gameObject.name}");
+//}
+
+// ----- 투영 화면 개념
+//if (Input.GetMouseButtonDown(0))
+//{
+//    Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+//    Vector3 dir = mousePos - Camera.main.transform.position;
+//    dir = dir.normalized;
+
+//    Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+
+//    RaycastHit hit;
+//    if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+//    {
+//        Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+//    }
+//}
