a4f99e6cd63a2342ac097c804ce4aa1cfe02792f
Johnny <pgisung@naver.com>
2024-05-18 23:44:28 +0900

    [New] add Game Server project

---
+++ Server/DummyClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList.First();
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World");
+                int sendBytes = socket.Send(sendBuff);
+
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
