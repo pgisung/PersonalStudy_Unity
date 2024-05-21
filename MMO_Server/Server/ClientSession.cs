2f1a82b6771520b749d43a0dab5b644f3db2d696
Johnny <pgisung@naver.com>
2024-05-21 23:47:46 +0900

    [Add] Packet generator

---
+++ MMO_Server/Server/ClientSession.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerCore;
+
+namespace Server
+{
+    class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected: {endPoint}");
+
+            //Packet packet = new Packet() { size = 100, packetId = 10 };
+
+            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            //byte[] buffer = BitConverter.GetBytes(packet.size);
+            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
+            //Send(sendBuff);
+            Thread.Sleep(5000);
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
